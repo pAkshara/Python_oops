bef24d219f47d2984daed8830afa01b6ff71f343
pAkshara <64862382+pAkshara@users.noreply.github.com>
2020-07-16 17:58:06 +0530

    https://github.com/pAkshara/Advance-Python-Files

---
+++ application.py
@@ -0,0 +1,24 @@
+# Object oriented programming
+# Class --->It is a blue print or high level design of the program.
+# Methods ---> a function written inside a class is called as method.
+# Attribute ---> a variable written inside a class is called as attribute.
+# Self ---> It is a temparary place holder for an object.
+# Contructor ---> It is a initialization method.
+# Object ---> to enter are to acess any method we have to use object.
+
+# Parameters and arguments...
+# defining a method ---> parameter passing.....
+# arguments ---> while calling the method...
+
+# Oops....
+# Inhertence ----->
+                  # super class and sub class
+                  # parent (super class) and child (sub class)
+        # ex:- herting  the child class from parent class.
+# Encapsulation ----> encapsulate--- capsule
+              # ex:- protecting code from getting knowing at the user end
+# Polymorphism ----->
+                  # poly(many) + morph(forms or shape)
+            # ex:- car class can adopt many different types of models.
+# Abstraction ----> # template---
+            # ex:- knowing the details with out knowing the intension.
